40780b172fbb2cf4eba4bd6d5875b3a30a766e7b
Nelcione Dias <nelcionedias@gmail.com>
2023-01-06 10:25:23 -0300

    first commit

---
+++ src/main/java/projeto/loginSpringBoot/LoginSpringBootApplication.java
@@ -0,0 +1,15 @@
+package projeto.loginSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSpringBootApplication.class, args);
+		System.out.println("Olá mundo");
+
+	}
+
+}
---
+++ src/test/java/projeto/loginSpringBoot/LoginSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.loginSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
