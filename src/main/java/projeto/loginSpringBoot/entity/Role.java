26d95176a199872ac691d45c4f1c44947b1feaa8
Nelcione Dias <nelcionedias@gmail.com>
2023-01-06 11:04:08 -0300

    hotfix entity (Role)

--- src/main/java/projeto/loginSpringBoot/entity/Role.java
+++ src/main/java/projeto/loginSpringBoot/entity/Role.java
@@ -17,6 +17,8 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String name;
+    @ManyToMany(mappedBy = "roles")
     private List<User> users;
 }
